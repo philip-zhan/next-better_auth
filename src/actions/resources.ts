48e8a091821fc6034dd68d2b696277d954d5769e
Philip Zhan <h6zhan@gmail.com>
2025-10-02 21:21:41 -0700

    wip

---
+++ src/actions/resources.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import {
+  type NewResourceParams,
+  insertResourceSchema,
+  resources,
+} from "@/database/schema/resources";
+import { db } from "@/database/db";
+import { generateEmbeddings } from "@/lib/embedding";
+import { embeddings as embeddingsTable } from "@/database/schema/embeddings";
+
+export const createResource = async (input: NewResourceParams) => {
+  try {
+    const { content } = insertResourceSchema.parse(input);
+
+    const [resource] = await db
+      .insert(resources)
+      .values({ content: content })
+      .returning();
+
+    const embeddings = await generateEmbeddings(content);
+    await db.insert(embeddingsTable).values(
+      embeddings.map((embedding) => ({
+        resourceId: resource.id,
+        ...embedding,
+      }))
+    );
+
+    return "Resource successfully created and embedded.";
+  } catch (error) {
+    return error instanceof Error && error.message.length > 0
+      ? error.message
+      : "Error, please try again.";
+  }
+};
--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,4 +1,9 @@
-import { streamText, type UIMessage, convertToModelMessages } from "ai";
+import {
+  streamText,
+  type UIMessage,
+  convertToModelMessages,
+  stepCountIs,
+} from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -17,8 +22,10 @@ export async function POST(req: Request) {
   const result = streamText({
     model: webSearch ? "perplexity/sonar" : model,
     messages: convertToModelMessages(messages),
-    system:
-      "You are a helpful assistant that can answer questions and help with tasks",
+    stopWhen: stepCountIs(5),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
   });
 
   // send sources and reasoning back to the client
