af0582ccb31baeedadc3d75101a0d206143c7a89
Philip Zhan <h6zhan@gmail.com>
2025-10-04 21:25:43 -0700

    remove settings from side bar

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -3,7 +3,6 @@
 import {
   RiLineChartLine,
   RiToolsFill,
-  RiSettingsLine,
   RiSpeedUpLine,
   RiBankCardLine,
 } from "@remixicon/react";
@@ -41,11 +40,6 @@ const data = {
           title: "Knowledge Base",
           url: "/dashboard/knowledge-base",
           icon: RiToolsFill,
-        },
-        {
-          title: "Settings",
-          url: "/dashboard/settings",
-          icon: RiSettingsLine,
         },
         {
           title: "Billing",
