513623d21d8a9932c5d25b594168eabb43bcb72e
Philip Zhan <h6zhan@gmail.com>
2025-12-06 12:21:55 -0800

    remove ReactQueryDevtools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "sonner";
 import { authClient } from "@/lib/auth-client";
 import { useUploadThing } from "@/lib/uploadthing";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -74,7 +74,7 @@ export function Providers({ children }: { children: ReactNode }) {
           <Toaster />
         </AuthUIProvider>
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
